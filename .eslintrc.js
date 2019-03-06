e8d2ae2759e04ce4f82f6163662c7cbf882e6f40
Avery Lamp <averylamp@gmail.com>
2019-03-06 17:00:44 -0500

    Remade eslint to use js config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "no-underscore-dangle": "off",
+    "import/prefer-default-export": "off",
+    "camelcase" : [ "error", { "allow": ["_id", "_ids"]}]
+  },
+};
