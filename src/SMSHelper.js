8370ebc5e95cb5d08f8a7aa3469f9a4d501464a9
Avery Lamp <averylamp@gmail.com>
2019-03-07 17:54:07 -0500

    Switched to debugger logs

--- src/SMSHelper.js
+++ src/SMSHelper.js
@@ -2,6 +2,8 @@ const accountSid = 'AC78a2a002835fe186a8c6d290a27d308d';
 const authToken = '3dd7f2cbe6d56127f1a074123f9e05f0';
 const client = require('twilio')(accountSid, authToken);
 
+// const debug = require('debug')('dev:SMSHelper');
+
 const sendMessage = (to, body) => {
   client.messages
     .create({
