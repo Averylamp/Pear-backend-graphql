effbd0c8efa8cd581a39800abe37ac73f607ddb7
briangu33 <briangu33@gmail.com>
2019-03-07 23:56:12 -0500

    WIP discovery graphql endpoints with no error handling

--- src/resolvers/DiscoveryQueueResolver.js
+++ src/resolvers/DiscoveryQueueResolver.js
@@ -1,17 +1,27 @@
-
 import { User } from '../models/UserModel';
-import { UserProfile } from '../models/UserProfileModel';
+import { DiscoveryQueue, DiscoveryItem } from '../models/DiscoveryQueueModel';
+
+const debug = require('debug')('dev:DiscoveryQueueResolver');
 
 
 export const resolvers = {
   Query: {
-
+    feed: async (_, { user_id }) => {
+      debug(`Getting feed for user with id: ${user_id}`);
+      return DiscoveryQueue.findOne({ user_id });
+    },
+  },
+  Mutation: {
+    addToQueue: async (_, { user_id, addedUser_id }) => DiscoveryQueue.findOne({ user_id })
+      .then((discoveryQueue) => {
+        discoveryQueue.currentDiscoveryItems.push(new DiscoveryItem({ user_id: addedUser_id }));
+        discoveryQueue.save();
+      }).then(() => ({ success: true })),
   },
   DiscoveryQueue: {
     user: async ({ user_id }) => User.findById(user_id),
   },
   DiscoveryItem: {
-    user: async ({ user_id }) => UserProfile.findById(user_id),
-    userProfile: async ({ userProfile_id }) => User.findById(userProfile_id),
+    user: async ({ user_id }) => User.findById(user_id),
   },
 };
