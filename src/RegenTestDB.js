85cd30f1c10df615d6b6b8ffdc6081fe0df0a49b
briangu33 <briangu33@gmail.com>
2019-03-10 00:06:14 -0500

    add regenTestDBMode

--- src/RegenTestDB.js
+++
@@ -1,8 +0,0 @@
-import 'babel-core/register';
-import 'babel-polyfill';
-import { start } from './start';
-
-const debug = require('debug')('dev:RegenTestDB');
-
-debug('Starting...');
-start(true);
