5e9ae767e285ac9c56920a9a63d90e6507251fa2
briangu33 <briangu33@gmail.com>
2019-03-12 22:45:09 -0400

    make linter happy

---
+++ src/tests/AttachProfileVars.js
@@ -0,0 +1,53 @@
+export const ATTACH_AVERY1_PROFILE_VARIABLES = {
+  user_id: '5c82162afec46c84e924a332',
+  detachedProfile_id: '5c82162afec46c84e9241112',
+  creatorUser_id: '5c82162afec46c84e924a336',
+};
+
+export const ATTACH_BRIAN1_PROFILE_VARIABLES = {
+  user_id: '5c82162afec46c84e924a333',
+  detachedProfile_id: '5c82162afec46c84e9241113',
+  creatorUser_id: '5c82162afec46c84e924a335',
+};
+
+export const ATTACH_JOSH1_PROFILE_VARIABLES = {
+  user_id: '5c82162afec46c84e924a334',
+  detachedProfile_id: '5c82162afec46c84e9241114',
+  creatorUser_id: '5c82162afec46c84e924a338',
+};
+
+export const ATTACH_SAMMI1_PROFILE_VARIABLES = {
+  user_id: '5c82162afec46c84e924a335',
+  detachedProfile_id: '5c82162afec46c84e9241115',
+  creatorUser_id: '5c82162afec46c84e924a334',
+};
+
+export const ATTACH_SAMMI2_PROFILE_VARIABLES = {
+  user_id: '5c82162afec46c84e924a335',
+  detachedProfile_id: '5c82162afec46c84e9241116',
+  creatorUser_id: '5c82162afec46c84e924a333',
+};
+
+export const ATTACH_MADE1_PROFILE_VARIABLES = {
+  user_id: '5c82162afec46c84e924a336',
+  detachedProfile_id: '5c82162afec46c84e9241117',
+  creatorUser_id: '5c82162afec46c84e924a334',
+};
+
+export const ATTACH_UMA1_PROFILE_VARIABLES = {
+  user_id: '5c82162afec46c84e924a337',
+  detachedProfile_id: '5c82162afec46c84e9241118',
+  creatorUser_id: '5c82162afec46c84e924a333',
+};
+
+export const ATTACH_SOPHIA1_PROFILE_VARIABLES = {
+  user_id: '5c82162afec46c84e924a338',
+  detachedProfile_id: '5c82162afec46c84e9241119',
+  creatorUser_id: '5c82162afec46c84e924a335',
+};
+
+export const ATTACH_SOPHIA2_PROFILE_VARIABLES = {
+  user_id: '5c82162afec46c84e924a338',
+  detachedProfile_id: '5c82162afec46c84e9241120',
+  creatorUser_id: '5c82162afec46c84e924a337',
+};
