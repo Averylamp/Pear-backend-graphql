436bafa17e8412343e7cf603912f39a768b32085
Avery Lamp <averylamp@gmail.com>
2019-03-18 05:29:42 -0400

    Uploading images script working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import 'babel-core/register';
 import 'babel-polyfill';
-import { start } from './start';
+// import { start } from './start';
+import { uploadImagesFromFolder } from './tests/ImageUploads';
 
 const debug = require('debug')('dev:Index');
 
 debug('Starting...');
-start();
+
+uploadImagesFromFolder('avery');
+
+// start();
