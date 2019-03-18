20f9a9123984add3349cedde686c4246854a3aa4
briangu33 <briangu33@gmail.com>
2019-03-17 23:11:53 -0400

    isDiscovering -> isSeeking

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -172,7 +172,7 @@ type User {
   school: String
   schoolEmail: String
   schoolEmailVerified: Boolean
-  isDiscovering: Boolean!
+  isSeeking: Boolean!
 
   # The ordered images that currently make up the User's Profile
   displayedImages: [ImageContainer!]!
@@ -293,7 +293,7 @@ const UserSchema = new Schema({
   school: { type: String, required: false },
   schoolEmail: { type: String, required: false },
   schoolEmailVerified: { type: Boolean, required: false, default: false },
-  isDiscovering: { type: Boolean, required: true, default: false },
+  isSeeking: { type: Boolean, required: true, default: false },
 
   pearPoints: { type: Number, required: true, default: 0 },
 
