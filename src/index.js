034db9f9fd524232b273703f31cede2c52d5d221
Avery Lamp <averylamp@gmail.com>
2019-03-18 05:50:49 -0400

    Running test infrastructure added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import 'babel-core/register';
 import 'babel-polyfill';
-// import { start } from './start';
+import { start } from './start';
 import { uploadImagesFromFolder } from './tests/ImageUploads';
 
 const debug = require('debug')('dev:Index');
 
 debug('Starting...');
 
-uploadImagesFromFolder('avery');
+let devMode = false;
+let regenTestDBMode = false;
+if (process.env.DEV === 'true') {
+  devMode = true;
+  debug('Dev detected');
+  if (process.env.REGENDB === 'true') {
+    debug('Regendb detected');
+    regenTestDBMode = true;
+  }
+}
 
-// start();
+
+if (devMode && regenTestDBMode) {
+  debug('Prepared to run tests in 5 seconds');
+  setTimeout(async () => {
+    debug('Running Tests');
+    const w = await uploadImagesFromFolder('avery');
+    debug(w);
+    process.exit(1);
+  }, 5000);
+}
+
+start();
