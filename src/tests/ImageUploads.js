a1080fc0c148b276c94e8b971049c932bc8492f0
Avery Lamp <averylamp@gmail.com>
2019-03-18 22:30:33 -0400

    Added mutation result checks for success

--- src/tests/ImageUploads.js
+++ src/tests/ImageUploads.js
@@ -1,16 +1,17 @@
 const debug = require('debug')('dev:tests:ImageUpload');
+const errorLog = require('debug')('dev:error:ImageUpload');
 const fs = require('fs');
 const path = require('path');
 const request = require('request');
 
-export const uploadImagesFromFolder = async function
-uploadImagesFromFolder(folder, uploadedByUser_id) {
+export const uploadImagesFromFolder = async (folder, uploadedByUser_id) => {
   const filePath = path.join(__dirname, `testImages/${folder}`);
   const imageMetadataPromises = [];
 
   return new Promise((fsResolve, fsReject) => {
     fs.readdir(filePath, (err, items) => {
       if (err) {
+        errorLog(err);
         fsReject(Error('Image folder not found'));
       }
       // items = [items[0]];
@@ -18,15 +19,15 @@ uploadImagesFromFolder(folder, uploadedByUser_id) {
         debug(`Uploading image for ${folder}: ${item}`);
         const itemPath = path.join(__dirname, `testImages/${folder}/${item}`);
         const base64Image = fs.readFileSync(itemPath, { encoding: 'base64' });
-        const newPromise = new Promise((resolve) => {
+        const newPromise = new Promise((resolve, reject) => {
           request.post('http://koala.mit.edu:1337/upload_image', {
             json: {
               image: base64Image,
             },
           }, (error, res, body) => {
             if (error) {
-              debug(error);
-              resolve(error);
+              errorLog(error);
+              reject(error);
             }
             debug(`Finished uploading image: ${item}`);
             resolve(body);
@@ -35,21 +36,18 @@ uploadImagesFromFolder(folder, uploadedByUser_id) {
         imageMetadataPromises.push(newPromise);
       });
 
-      const result = Promise.all(imageMetadataPromises).then((imageResults) => {
+      return Promise.all(imageMetadataPromises).then((imageResults) => {
         const imageMetadata = [];
         imageResults.forEach((imageResult) => {
-          if (imageResult instanceof Error) {
-            debug(`Failed to upload image: ${imageResult}`);
-            fsReject(Error('At least one image failed to upload'));
-          } else {
-            const finalImageResult = imageResult;
-            finalImageResult.uploadedByUser_id = uploadedByUser_id;
-            imageMetadata.push(finalImageResult);
-          }
+          const finalImageResult = imageResult;
+          finalImageResult.uploadedByUser_id = uploadedByUser_id;
+          imageMetadata.push(finalImageResult);
         });
-        return imageResults;
+        fsResolve(imageResults);
+      }).catch((allImagesError) => {
+        errorLog(allImagesError);
+        fsReject(allImagesError);
       });
-      fsResolve(result);
     });
   });
 };
