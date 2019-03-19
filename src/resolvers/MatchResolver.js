84793eeb425a539a35c1db67b0fc54daa7d3890b
briangu33 <briangu33@gmail.com>
2019-03-19 01:30:32 -0400

    add match resolver types

--- src/resolvers/MatchResolver.js
+++ src/resolvers/MatchResolver.js
@@ -1,8 +1,13 @@
 import { User } from '../models/UserModel';
 
 export const resolvers = {
-  Query: {
-
+  Mutation: {
+    matchmakerCreateRequest: async () => {},
+    personalCreateRequest: async () => {},
+    viewRequest: async () => {},
+    acceptRequest: async () => {},
+    rejectRequest: async () => {},
+    unmatch: async () => {},
   },
   Match: {
     sentByUser: async ({ sentByUser_id }) => User.findById(sentByUser_id),
