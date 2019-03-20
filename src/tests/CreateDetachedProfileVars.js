e0d3971d93e71e301589e3431b6061834017902f
Avery Lamp <averylamp@gmail.com>
2019-03-19 22:41:40 -0400

    Refactor test scripts to use photos uploaded

--- src/tests/CreateDetachedProfileVars.js
+++ src/tests/CreateDetachedProfileVars.js
@@ -1,8 +1,3 @@
-import {
-  avery1, brian1, josh1, sammi1, made1, uma1, sophia1,
-} from './ImageMetadata';
-
-
 export const CREATE_AVERY1_DETACHED_PROFILE_VARIABLES = {
   detachedProfileInput: {
     _id: '5c82162afec46c84e9241112',
@@ -17,7 +12,6 @@ export const CREATE_AVERY1_DETACHED_PROFILE_VARIABLES = {
     bio: 'Want to meet the real forbidden fruit? Attracted to ~ mysterious ~ guys who have multiple ~ layers ~ to them? Well that\'s literally Avery. Avery is also one of the most kind, honest people I\'ve ever met and probably THE BEST cook I\'ve ever met too. If nothing else, it wouldn\'t be a bad idea to marry him just for his pesto...',
     dos: ['ask him about his new dating app'],
     donts: ['be too clingy - sometimes he needs his space!!', 'plan a date for before 1PM'],
-    images: [avery1],
   },
 };
 
@@ -35,7 +29,6 @@ export const CREATE_BRIAN1_DETACHED_PROFILE_VARIABLES = {
     bio: 'Seeking a guy who will treat you right???? Well look no further than BRIAN GU\nMIT math boi, currently Boston based.\n\nGreatest accomplishments: can outfit you with the thriving apparel line he runs out of his dorm 😎, runs a meme page with 20k+ likes, has a diversity of interests aka frisbee/boulder/tennis/travel.\nCONS: failed out of 6th grade Chinese school, you\'ll have to compete with his stuffed animals for attention',
     dos: ['be direct (he hates guessing)', 'ask him about Mass Tech', 'go out for any Asian food!'],
     donts: ['take him the movies on a first date'],
-    images: [brian1],
   },
 };
 
@@ -53,7 +46,6 @@ export const CREATE_JOSH1_DETACHED_PROFILE_VARIABLES = {
     bio: 'Josh is literally one of the most talented people I’ve ever met. I thought he would peak in high school bc he was SO EXTRA (literally missed homecoming dance even though he was homecoming KING bc he was doing contracted research at Peking University when he was 15). Ask him about back up dancing for Hailee Steinfeld or Janet Jackson/his dance charity that he started in Boston/or his youtube videos that hit millions of views.',
     dos: ['ask him about dance', 'be intellectual (mans loves a good conversation)'],
     donts: ['try to go shot for shot with him (this man won\'t make it past three drinks)'],
-    images: [josh1],
   },
 };
 
@@ -71,7 +63,6 @@ export const CREATE_SAMMI1_DETACHED_PROFILE_VARIABLES = {
     bio: 'Artistic, sensitive, passionate, and also just super hilarious and down to earth. She’s one of those girls that can have a conversation with you about literally anything. She\'s a family oriented person and one of those people that no one can speak poorly of. <3',
     dos: ['CHECK OUT HER ART INSTA @ARTBYSAMMI it\'s 🔥🔥', 'be thoughtful', 'make the first move'],
     donts: ['just be looking for a hookup - she\'s not here for that'],
-    images: [sammi1],
   },
 };
 
@@ -89,7 +80,6 @@ export const CREATE_SAMMI2_DETACHED_PROFILE_VARIABLES = {
     bio: 'Sammi had e. coli poisoning in the 3rd grade and STILL went to EVERYDAY of class. That’s how much you can depend on her 😭. Now sammi is a SKister (sigma kappa sorority girl ew i know) at MIT where she studies things I don’t understand (computer something beep boop beep).',
     dos: ['ask her about her drawings (she\'s insta famous)', 'put effort into dates!'],
     donts: ['feed her peanuts (she\'s deathly allergic)'],
-    images: [],
   },
 };
 
@@ -107,7 +97,6 @@ export const CREATE_MADE1_DETACHED_PROFILE_VARIABLES = {
     bio: 'She’s one of the most hardworking and passionate people I’ve ever met! Not only is she hot (omg like come on look at her pics), but she is constantly working on side projects on her own (ask her about one of the many apps she’s made, clothing lines she started, her photography page, etc etc etc). We STAN a lady that’s hot, has class, and has brains. If you get inferiority complex and have masculinity issues when next to a boss lady then move on!',
     dos: ['take her out somewhere nice', 'be a gentleman', 'buy her mint oreos, those are her FAVORITE'],
     donts: ['be an asshole or too full of yourself', 'be too forward'],
-    images: [made1],
   },
 };
 
@@ -125,7 +114,6 @@ export const CREATE_UMA1_DETACHED_PROFILE_VARIABLES = {
     bio: 'This girl has it all: beauty, brains, and of course a resume packed with the hottest tech companies 🤑🤑. She\'s ambitious and brilliant but is also super grounded and a 10/10 caring friend. Catch her while she\'s still single because this deal is LIMITED 👏 TIME 👏 ONLY',
     dos: ['be able to hold a conversation', 'tell her about your favorite books', 'go to the gym with her'],
     donts: ['play games', 'be intimidated by her - she won\'t bite!'],
-    images: [uma1],
   },
 };
 
@@ -143,7 +131,6 @@ export const CREATE_SOPHIA1_DETACHED_PROFILE_VARIABLES = {
     bio: 'The ONLY person I know that is ALWAYS down for a spontaneous adventure or food run. Sophia embodies the work hard play hard mentality. Harvard PreMed (did someone say moneyyyy) but also fluent in alcohol (rip kidney). Hurry and slide into her dms before someone else does!!',
     dos: ['let her know when ur hungry because chances are she is too', 'have late night deep convos with her because she\'s a great listener and advice-giver'],
     donts: ['watch the bachelor without her', 'put beans in her nachos'],
-    images: [sophia1],
   },
 };
 
@@ -161,6 +148,5 @@ export const CREATE_SOPHIA2_DETACHED_PROFILE_VARIABLES = {
     bio: 'Like bad girls? Sophia\'s bad at everything.\n\nExcept watching movies and drinking copious amounts of boba. If you like either of those things, she\'s the one!',
     dos: ['Take her to the movies! she\'s a bit of a movie junkie and loves a good horror flick too, so you won\'t have to give those up.'],
     donts: ['bBe late to a date! Sophia is the most punctual person I know'],
-    images: [],
   },
 };
