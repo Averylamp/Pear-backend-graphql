526bda2ea8da1aaff4afa55da936a660df5bdd0e
Avery Lamp <averylamp@gmail.com>
2019-03-19 22:41:40 -0400

    Refactor test scripts to use photos uploaded

--- src/tests/UpdatePhotosVars.js
+++ src/tests/UpdatePhotosVars.js
@@ -1,12 +1,8 @@
-import {
-  avery1, brian1, josh1, sammi1, made1, uma1, sophia1,
-} from './ImageMetadata';
 
 export const ADD_AVERY_PHOTOS_VARIABLES = {
   updateUserPhotosInput: {
     user_id: '5c82162afec46c84e924a332',
     displayedImages: [
-      avery1,
     ],
     additionalImages: [
     ],
@@ -17,7 +13,6 @@ export const ADD_BRIAN_PHOTOS_VARIABLES = {
   updateUserPhotosInput: {
     user_id: '5c82162afec46c84e924a333',
     displayedImages: [
-      brian1,
     ],
     additionalImages: [
     ],
@@ -28,7 +23,6 @@ export const ADD_JOSH_PHOTOS_VARIABLES = {
   updateUserPhotosInput: {
     user_id: '5c82162afec46c84e924a334',
     displayedImages: [
-      josh1,
     ],
     additionalImages: [
     ],
@@ -39,7 +33,6 @@ export const ADD_SAMMI_PHOTOS_VARIABLES = {
   updateUserPhotosInput: {
     user_id: '5c82162afec46c84e924a335',
     displayedImages: [
-      sammi1,
     ],
     additionalImages: [
     ],
@@ -50,7 +43,6 @@ export const ADD_MADE_PHOTOS_VARIABLES = {
   updateUserPhotosInput: {
     user_id: '5c82162afec46c84e924a336',
     displayedImages: [
-      made1,
     ],
     additionalImages: [
     ],
@@ -61,7 +53,6 @@ export const ADD_UMA_PHOTOS_VARIABLES = {
   updateUserPhotosInput: {
     user_id: '5c82162afec46c84e924a337',
     displayedImages: [
-      uma1,
     ],
     additionalImages: [
     ],
@@ -72,7 +63,6 @@ export const ADD_SOPHIA_PHOTOS_VARIABLES = {
   updateUserPhotosInput: {
     user_id: '5c82162afec46c84e924a338',
     displayedImages: [
-      sophia1,
     ],
     additionalImages: [
     ],
