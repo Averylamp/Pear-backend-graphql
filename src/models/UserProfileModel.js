7a9fde3390907e7dc0f22c7f5212f9108639f348
briangu33 <briangu33@gmail.com>
2019-03-22 19:27:17 -0400

    add new types

--- src/models/UserProfileModel.js
+++ src/models/UserProfileModel.js
@@ -32,9 +32,23 @@ type UserProfileMutationResponse{
 }
 `;
 
+const endorsementEdgeType = `
+type EndorsementEdge {
+  _id: ID!
+  active: Boolean!
+  otherUser_id: ID!
+  otherUser: User
+  
+  myProfile_id: ID
+  myProfile: UserProfile
+  theirProfile_id: ID
+  theirProfile: UserProfile
+}
+`;
 
 export const typeDef = userProfileType
-+ userProfileMutationResponse;
++ userProfileMutationResponse
++ endorsementEdgeType;
 
 
 const UserProfileSchema = new Schema({
@@ -48,6 +62,13 @@ const UserProfileSchema = new Schema({
   dos: { type: [String], required: true },
   donts: { type: [String], required: true },
 }, { timestamps: true });
+
+export const EndorsementEdgeSchema = new Schema({
+  active: { type: Schema.Types.Boolean, required: true, default: true },
+  otherUser_id: { type: Schema.Types.ObjectId, required: true, index: true },
+  myProfile_id: { type: Schema.Types.ObjectId, required: true, index: true },
+  theirProfile_id: { type: Schema.Types.ObjectId, required: true, index: true },
+});
 
 
 export const UserProfile = mongoose.model('UserProfile', UserProfileSchema);
