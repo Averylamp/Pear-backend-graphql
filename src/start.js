1a6728e8f5e2599a43efcf91d7be1f292add1451
briangu33 <briangu33@gmail.com>
2019-03-21 21:06:51 -0400

    fix bug where we weren't pushing to previousDiscoveryItems, rename to historyDiscoveryItems

--- src/start.js
+++ src/start.js
@@ -76,7 +76,7 @@ if (regenTestDBMode) {
     process.exit(1);
   }
 }
-const tracing = process.env.PERF === 'true' ? true : false;
+const tracing = process.env.PERF === 'true';
 if (tracing) debug('Perf mode detected');
 
 const URL = 'http://localhost';
