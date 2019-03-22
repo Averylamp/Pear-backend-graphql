398bf5f9d8e2bce8ce8c5361dcd17b1c68253fe4
briangu33 <briangu33@gmail.com>
2019-03-22 19:27:17 -0400

    add new types

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -2,6 +2,7 @@ import { MatchingDemographicsSchema, MatchingPreferencesSchema } from './Matchin
 import { GeoJSONSchema } from './TypeSchemas';
 import { ImageContainerSchema } from './ImageSchemas';
 import { EdgeSummarySchema } from './MatchModel';
+import { EndorsementEdgeSchema } from './UserProfileModel';
 
 const mongoose = require('mongoose');
 
@@ -184,6 +185,9 @@ type User {
   detachedProfile_ids: [ID!]!
   # All Detached Profiles for a user
   detachedProfileObjs: [DetachedProfile!]!
+  
+  # metainfo about matchmaker chats
+  endorsementEdges: [EndorsementEdge!]!
 
   discoveryQueue_id: ID!
   discoveryQueueObj: DiscoveryQueue!
@@ -288,6 +292,10 @@ const UserSchema = new Schema({
   },
   detachedProfile_ids: {
     type: [Schema.Types.ObjectId], required: true, index: true, default: [],
+  },
+
+  endorsementEdges: {
+    type: [EndorsementEdgeSchema], required: true, default: [],
   },
 
   discoveryQueue_id: { type: Schema.Types.ObjectId, required: true },
