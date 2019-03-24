92f9db0cde647f07f3bd3449f534cfdfb9de2d2c
briangu33 <briangu33@gmail.com>
2019-03-23 20:27:39 -0400

    add location types

--- src/models/TypeSchemas.js
+++ src/models/TypeSchemas.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-export const GeoJSONSchema = new Schema({
+export const PointSchema = new Schema({
   type: {
     type: String,
     enum: ['Point'],
     required: true,
   },
   coordinates: {
-    type: [Number],
+    type: [Number], // [<longitude>, <latitude>]
     required: true,
   },
 });
