216127a07083b93ce791243aed9e5233133e2f23
briangu33 <briangu33@gmail.com>
2019-03-24 22:31:38 -0400

    add remaining server messages

--- src/FirebaseManager.js
+++ src/FirebaseManager.js
@@ -56,15 +56,31 @@ export const createEndorsementChat = ({ documentID, firstPerson, secondPerson })
       secondPersonLastOpened: new Date(0),
       access: [firstPerson.firebaseAuthID, secondPerson.firebaseAuthID],
     })
-    .then((docRef) => {
-      debug(`created document ${docRef}`);
-    })
     .catch((err) => {
       errorLog(`error creating chat document: ${err}`);
       throw err;
     });
 };
 
+export const createMatchChat = ({ documentID, firstPerson, secondPerson }) => {
+  const db = getFirebaseDb();
+  return db.collection('chats-brian')
+    .doc(documentID)
+    .set({
+      documentID,
+      type: 'MATCH',
+      firstPerson_id: firstPerson._id.toString(),
+      secondPerson_id: secondPerson._id.toString(),
+      lastActivity: new Date(),
+      firstPersonLastOpened: new Date(0),
+      secondPersonLastOpened: new Date(0),
+      access: [firstPerson.firebaseAuthID, secondPerson.firebaseAuthID],
+    })
+    .catch((err) => {
+      errorLog(`error creating chat document: ${err}`);
+      throw err;
+    });
+};
 
 export const sendNewEndorsementMessage = ({ chatID, endorser, endorsee }) => {
   const db = getFirebaseDb();
@@ -82,5 +98,84 @@ export const sendNewEndorsementMessage = ({ chatID, endorser, endorsee }) => {
     })
     .catch((err) => {
       errorLog(`error sending new endorsement message: ${err}`);
+    });
+};
+
+export const sendMatchRequestServerMessage = ({ chatID, initiator, hasMatchmaker }) => {
+  const db = getFirebaseDb();
+  const newMessageRef = db.collection('chats-brian')
+    .doc(chatID)
+    .collection('messages')
+    .doc();
+  const messageText = hasMatchmaker
+    ? `${initiator.firstName} sent both of you a match request.`
+    : `${initiator.firstName} sent a match request.`;
+  return newMessageRef
+    .set({
+      documentID: newMessageRef.id,
+      type: 'SERVER_MESSAGE',
+      contentType: 'TEXT',
+      content: messageText,
+      timestamp: new Date(),
+    })
+    .catch((err) => {
+      errorLog(`error sending new match request server message: ${err}`);
+    });
+};
+
+export const notifyEndorsementChatNewRequest = ({ chatID, sentBy, sentFor, receivedBy }) => {
+  const now = new Date();
+  const db = getFirebaseDb();
+  const chatDocRef = db.collection('chats-brian').doc(chatID);
+  const newMessageRef = chatDocRef.collection('messages').doc();
+  return newMessageRef
+    .set({
+      documentID: newMessageRef.id,
+      type: 'SERVER_MESSAGE',
+      contentType: 'TEXT',
+      content: `${sentBy.firstName} wants to Pear ${sentFor.firstName} with ${receivedBy.firstName}.`,
+      timestamp: now,
+    })
+    .then(() => (chatDocRef.set({ lastActivity: now }, { merge: true })))
+    .catch((err) => {
+      errorLog(`error notifying endorsement chat of new request: ${err}`);
+    });
+};
+
+export const sendMatchAcceptedServerMessage = ({ chatID }) => {
+  const now = new Date();
+  const db = getFirebaseDb();
+  const chatDocRef = db.collection('chats-brian').doc(chatID);
+  const newMessageRef = chatDocRef.collection('messages').doc();
+  return newMessageRef
+    .set({
+      documentID: newMessageRef.id,
+      type: 'SERVER_MESSAGE',
+      contentType: 'TEXT',
+      content: 'Chat request accepted.',
+      timestamp: now,
+    })
+    .then(() => (chatDocRef.set({ lastActivity: now }, { merge: true })))
+    .catch((err) => {
+      errorLog(`error sending match accepted chat message: ${err}`);
+    });
+};
+
+export const notifyEndorsementChatAcceptedRequest = ({ chatID, sentBy, sentFor, receivedBy }) => {
+  const now = new Date();
+  const db = getFirebaseDb();
+  const chatDocRef = db.collection('chats-brian').doc(chatID);
+  const newMessageRef = chatDocRef.collection('messages').doc();
+  return newMessageRef
+    .set({
+      documentID: newMessageRef.id,
+      type: 'SERVER_MESSAGE',
+      contentType: 'TEXT',
+      content: `${sentBy.firstName} Peared ${sentFor.firstName} with ${receivedBy.firstName}!`,
+      timestamp: now,
+    })
+    .then(() => (chatDocRef.set({ lastActivity: now }, { merge: true })))
+    .catch((err) => {
+      errorLog(`error sending match accepted chat message: ${err}`);
     });
 };
