59737377d0ee000ed9d116059ff5bade7f739769
briangu33 <briangu33@gmail.com>
2019-04-12 21:19:13 -0400

    linter

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,38 @@
-export const MAX_FEED_LENGTH = 10;
+export const MAX_FEED_LENGTH = 18;
 
-export const INITIALIZED_FEED_LENGTH = 3;
+export const INITIALIZED_FEED_LENGTH = 5;
+
+export const NEW_PROFILE_BONUS = 2;
 
 export const TICK_LENGTH_MILLIS = 1000 * 60;
 
-export const EXPECTED_TICKS_PER_NEW_PROFILE = 60;
+export const EXPECTED_TICKS_PER_NEW_PROFILE = 240;
 
-export const STAT_SNAPSHOT_GENERATION_TIME = 15 * 1000;
+export const STAT_SNAPSHOT_GENERATION_TIME = 30 * 60 * 1000;
+
+export const FUZZY_SCHOOL_LIST = [
+  'Harvard',
+  'harvard',
+  'Harvard University',
+  'Harvard College',
+  'harvard university',
+  'harvard college',
+  'Harvard university',
+  'Harvard college',
+  'MIT',
+  'mit',
+  'M.I.T.',
+  'M.I.T',
+  'Massachusetts Institute of Technology',
+  'massachusetts institute of technology',
+  'Mass Tech',
+  'mass tech',
+  'Stanford',
+  'stanford',
+  'Stanford University',
+  'Wellesley',
+  'wellesley',
+  'Wellesley College',
+  'wellesley college',
+  'Wellesley college',
+];
