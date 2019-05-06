6358265cdb1877cbc915c2eaac9dd5cb84bfe50e
briangu33 <briangu33@gmail.com>
2019-05-06 03:15:38 -0400

    make createNewMatch remove discovery item, add skipDiscoveryItem

--- src/resolvers/MatchingResolvers/CreateNewMatch.js
+++ src/resolvers/MatchingResolvers/CreateNewMatch.js
@@ -1,25 +1,23 @@
-import nanoid from 'nanoid';
-import { receiveRequest, sendRequest, User } from '../models/UserModel';
-import { createMatchObject, Match } from '../models/MatchModel';
-import {
-  createMatchChat,
-  getChatDocPathFromId,
-  notifyEndorsementChatAcceptedRequest,
-  notifyEndorsementChatNewRequest,
-  sendMatchAcceptedMatchmakerPushNotification,
-  sendMatchAcceptedPushNotification,
-  sendMatchAcceptedServerMessage, sendMatchmakerRequestMessage,
-  sendMatchReceivedByPushNotification,
-  sendMatchSentForPushNotification, sendPersonalRequestMessage,
-} from '../FirebaseManager';
+import { receiveRequest, sendRequest, User } from '../../models/UserModel';
 import {
   GET_USER_ERROR,
   SEND_MATCH_REQUEST_ERROR, USERS_ALREADY_MATCHED_ERROR,
   WRONG_CREATOR_ERROR,
-} from '../resolvers/ResolverErrorStrings';
-import { generateSentryErrorForResolver } from '../SentryHelper';
-import { DiscoveryQueue } from '../models/DiscoveryQueueModel';
-import { rollbackObject } from '../../util/util';
+} from '../ResolverErrorStrings';
+import { DiscoveryQueue } from '../../models/DiscoveryQueueModel';
+import nanoid from 'nanoid';
+import {
+  createMatchChat,
+  getChatDocPathFromId,
+  notifyEndorsementChatNewRequest,
+  sendMatchmakerRequestMessage,
+  sendMatchReceivedByPushNotification,
+  sendMatchSentForPushNotification,
+  sendPersonalRequestMessage,
+} from '../../FirebaseManager';
+import { createMatchObject } from '../../models/MatchModel';
+import { rollbackObject } from '../../../util/util';
+import { generateSentryErrorForResolver } from '../../SentryHelper';
 
 const debug = require('debug')('dev:Matching');
 const errorLogger = require('debug')('error:Matching');
@@ -32,32 +30,7 @@ const errorLog = log => errorLogger(errorStyling(log));
 
 const mongoose = require('mongoose');
 
-// utility function for rolling back a specific db operation on unsuccessful unmatches and
-// accept/reject ops
-export const rollbackEdgeUpdates = (rollbackSummary) => {
-  for (const rollbackItem of rollbackSummary) {
-    const updateObj = {
-      'edgeSummaries.$[element].edgeStatus': rollbackItem.rollbackEdgeStatus,
-      'edgeSummaries.$[element].lastStatusChange': rollbackItem.rollbackEdgeLastUpdated,
-    };
-    updateObj[rollbackItem.matchesListOp] = {
-      currentMatch_ids: rollbackItem.match_id,
-    };
-    debug(`rollbackEdgeUpdate: ${updateObj}`);
-    User.findByIdAndUpdate(rollbackItem.user_id, updateObj, {
-      new: true,
-      arrayFilters: [{ 'element.match_id': rollbackItem.match_id }],
-    }, (err) => {
-      if (err) {
-        debug(`Failed to roll back my edge status updates: ${err.toString()}`);
-      } else {
-        debug('Rolled back my edge status updates');
-      }
-    });
-  }
-};
-
-export const createNewMatch = async ({
+export const createNewMatchResolver = async ({
   sentByUser_id, sentForUser_id, receivedByUser_id, _id = mongoose.Types.ObjectId(), requestText,
 }) => {
   const matchID = _id;
@@ -334,181 +307,5 @@ export const createNewMatch = async ({
   return {
     success: true,
     match,
-  };
-};
-
-export const decideOnMatch = async ({ user_id, match_id, decision }) => {
-  if (!['reject', 'accept'].includes(decision)) {
-    throw new Error(`Unknown match action: ${decision}`);
-  }
-  const acceptedMatch = decision === 'accept';
-  // verifies that user exists, match exists, user is part of match, user hasn't yet taken
-  // accept/reject action on match
-  const promisesResult = await getAndValidateUserAndMatchObjects(
-    {
-      user_id,
-      match_id,
-      validationType: decision,
-    },
-  );
-  const user = promisesResult[0];
-  const match = promisesResult[1];
-  const otherUser = promisesResult[2];
-  const initialUser = user.toObject();
-  const initialMatch = match.toObject();
-  const initialOtherUser = otherUser.toObject();
-
-  // set object field names depending on whether user_id is sentFor or receivedBy in match
-  const imSentFor = (user_id === match.sentForUser_id.toString());
-  const myStatusKeyName = imSentFor ? 'sentForUserStatus' : 'receivedByUserStatus';
-  const theirStatusKeyName = imSentFor ? 'receivedByUserStatus' : 'sentForUserStatus';
-  const myStatusLastUpdatedKeyName = imSentFor
-    ? 'sentForUserStatusLastUpdated'
-    : 'receivedByUserStatusLastUpdated';
-
-  // update my status in the match object
-  const matchUpdateObj = {};
-  matchUpdateObj[myStatusKeyName] = acceptedMatch ? 'accepted' : 'rejected';
-  matchUpdateObj[myStatusLastUpdatedKeyName] = new Date();
-
-  // update the match object
-  const matchUpdated = await Match.findByIdAndUpdate(match_id, matchUpdateObj,
-    { new: true })
-    .exec()
-    .catch(err => err);
-
-  // remove the match_id reference in my requestedMatch_ids array
-  const mePullRequestUpdate = await User.findByIdAndUpdate(user_id, {
-    $pull: {
-      requestedMatch_ids: match_id,
-    },
-  }, { new: true })
-    .exec()
-    .catch(err => err);
-
-  // update the edge objects and push the match_id ref to the currentMatch_ids of both users
-  // if the other user has also already made a decision
-  let edgeUpdate = null;
-  let isAMatch = false;
-  const theirMatchStatus = matchUpdated[theirStatusKeyName];
-  if (['accepted', 'rejected'].includes(theirMatchStatus)) {
-    if (theirMatchStatus === 'accepted' && acceptedMatch) {
-      isAMatch = true;
-      edgeUpdate = await User.updateMany({
-        _id: { $in: [user_id, otherUser._id.toString()] },
-      }, {
-        $push: {
-          currentMatch_ids: match_id,
-        },
-        'edgeSummaries.$[element].edgeStatus': 'match',
-        'edgeSummaries.$[element].lastStatusChange': new Date(),
-      }, {
-        arrayFilters: [{ 'element.match_id': match_id }],
-      })
-        .exec()
-        .catch(err => err);
-    } else {
-      edgeUpdate = await User.updateMany({
-        _id: { $in: [user_id, otherUser._id.toString()] },
-      }, {
-        'edgeSummaries.$[element].edgeStatus': 'rejected',
-        'edgeSummaries.$[element].lastStatusChange': new Date(),
-      }, {
-        arrayFilters: [{ 'element.match_id': match_id }],
-      })
-        .exec()
-        .catch(err => err);
-    }
-  }
-
-  // roll back if any update failed
-  if (mePullRequestUpdate instanceof Error || edgeUpdate instanceof Error) {
-    debug('match decision failed, rolling back');
-    let message = '';
-    if (mePullRequestUpdate instanceof Error) {
-      message += mePullRequestUpdate.toString();
-    }
-    if (edgeUpdate instanceof Error) {
-      message += edgeUpdate.toString();
-    }
-    User.findByIdAndUpdate(user_id, initialUser, {
-      new: true,
-      overwrite: true,
-    }).exec()
-      .then(() => {
-        debug('rolled back me user object successfully');
-      })
-      .catch((err) => {
-        errorLog(`error rolling back me user object: ${err}`);
-      });
-    User.findByIdAndUpdate(otherUser._id.toString(), initialOtherUser, {
-      new: true,
-      overwrite: true,
-    }).exec()
-      .then(() => {
-        debug('rolled back other user object successfully');
-      })
-      .catch((err) => {
-        errorLog(`error rolling back other user object: ${err}`);
-      });
-    Match.findByIdAndUpdate(match_id, initialMatch, {
-      new: true,
-      overwrite: true,
-    }).exec()
-      .then(() => {
-        debug('rolled back match object successfully');
-      })
-      .catch((err) => {
-        errorLog(`error rolling back match object: ${err}`);
-      });
-    return {
-      success: true,
-      message,
-    };
-  }
-  if (isAMatch) {
-    sendMatchAcceptedServerMessage({ chatID: match.firebaseChatDocumentID });
-    sendMatchAcceptedPushNotification({ user, otherUser });
-    sendMatchAcceptedPushNotification({ otherUser, user });
-    const matchmakerMade = match.sentForUser_id.toString() !== match.sentByUser_id.toString();
-    if (matchmakerMade) {
-      const sentBy = await User.findById(match.sentByUser_id);
-      let sentFor;
-      let receivedBy;
-      if (imSentFor) {
-        sentFor = user;
-        receivedBy = otherUser;
-      } else {
-        sentFor = otherUser;
-        receivedBy = user;
-      }
-      const endorsementEdges = sentBy.endorsementEdges.filter(
-        edge => edge.otherUser_id.toString() === sentFor._id.toString(),
-      );
-      if (endorsementEdges.length > 0) {
-        const [endorsementEdge] = endorsementEdges;
-        const endorsementChatId = endorsementEdge.firebaseChatDocumentID;
-        notifyEndorsementChatAcceptedRequest({
-          chatID: endorsementChatId,
-          sentBy,
-          sentFor,
-          receivedBy,
-        });
-      }
-      sendMatchAcceptedMatchmakerPushNotification({ sentBy, sentFor });
-      // increment pear points
-      // call .exec() to make sure this query/update actually runs, since the result isn't used
-      // anywhere
-      // it's not a big deal if pear points update fails, so this isn't included in rollback
-      User.findByIdAndUpdate(match.sentByUser_id, {
-        $inc: {
-          pearPoints: 1,
-        },
-      }).exec();
-    }
-  }
-  return {
-    success: true,
-    match: matchUpdated,
   };
 };
