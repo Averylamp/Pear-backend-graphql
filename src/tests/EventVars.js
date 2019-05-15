60645b794958ebf973e278df92b867283c0cd338
briangu33 <briangu33@gmail.com>
2019-05-15 12:48:39 -0400

    add event codes and tests

---
+++ src/tests/EventVars.js
@@ -0,0 +1,21 @@
+import { BRIAN_ID, EVENT0_ID, MADE_ID } from './TestsContants';
+
+export const CREATE_EVENT0_VARIABLES = {
+  eventInput: {
+    _id: EVENT0_ID,
+    code: 'TESTCODE',
+    name: 'Test Event 5/15',
+    startTime: '1557935220000',
+    endTime: '1559935220000',
+  },
+};
+
+export const ADD_BRIAN_EVENT0 = {
+  user_id: BRIAN_ID,
+  code: 'TESTCODE',
+};
+
+export const ADD_MADE_EVENT0 = {
+  user_id: MADE_ID,
+  code: 'TESTCODE',
+};
