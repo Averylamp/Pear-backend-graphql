ed583be24d83cfcf8dfe76b7d922f79ec21c7baa
briangu33 <briangu33@gmail.com>
2019-05-17 00:03:27 -0400

    add new demographic info types

--- src/models/MatchingSchemas.js
+++ src/models/MatchingSchemas.js
@@ -5,10 +5,115 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const matchingDemographicsType = `
-type MatchingDemographics{
+type MatchingDemographics {
   gender: Gender
   age: Int
   location: Location
+  ethnicity: EthnicityInfo
+  educationLevel: EducationLevelInfo
+  religion: ReligionInfo
+  politicalView: PoliticsInfo
+  drinking: DrinkingInfo
+  smoking: SmokingInfo
+  cannabis: CannabisInfo
+  drugs: DrugsInfo
+}
+
+enum EthnicityEnum {
+  AMERICAN_INDIAN
+  BLACK_AFRICAN_DESCENT
+  EAST_ASIAN
+  HISPANIC_LATINO
+  MIDDLE_EASTERN
+  PACIFIC_ISLANDER
+  SOUTH_ASIAN
+  WHITE_CAUCASIAN
+  OTHER
+  PREFER_NOT_TO_SAY
+}
+
+enum EducationLevelEnum {
+  HIGH_SCHOOL
+  UNDERGRAD
+  POSTGRAD
+  PREFER_NOT_TO_SAY
+}
+
+enum ReligionEnum {
+  BUDDHIST
+  CATHOLIC
+  CHRISTIAN
+  HINDU
+  JEWISH
+  MUSLIM
+  SPIRITUAL
+  AGNOSTIC
+  ATHEIST
+  OTHER
+  PREFER_NOT_TO_SAY
+}
+
+enum PoliticsEnum {
+  LIBERAL
+  MODERATE
+  CONSERVATIVE
+  OTHER
+  PREFER_NOT_TO_SAY
+}
+
+enum HabitsEnum {
+  YES
+  SOMETIMES
+  NO
+  PREFER_NOT_TO_SAY
+}
+
+type EthnicityInfo {
+  response: [EthnicityEnum!]!
+  visible: Boolean!
+  userHasResponded: Boolean!
+}
+
+type EducationLevelInfo {
+  response: [EducationLevelEnum!]!
+  visible: Boolean!
+  userHasResponded: Boolean!
+}
+
+type ReligionInfo {
+  response: [ReligionEnum!]!
+  visible: Boolean!
+  userHasResponded: Boolean!
+}
+
+type PoliticsInfo {
+  response: [PoliticsEnum!]!
+  visible: Boolean!
+  userHasResponded: Boolean!
+}
+
+type DrinkingInfo {
+  response: [HabitsEnum!]!
+  visible: Boolean!
+  userHasResponded: Boolean!
+}
+
+type SmokingInfo {
+  response: [HabitsEnum!]!
+  visible: Boolean!
+  userHasResponded: Boolean!
+}
+
+type CannabisInfo {
+  response: [HabitsEnum!]!
+  visible: Boolean!
+  userHasResponded: Boolean!
+}
+
+type DrugsInfo {
+  response: [HabitsEnum!]!
+  visible: Boolean!
+  userHasResponded: Boolean!
 }
 `;
 
@@ -23,6 +128,72 @@ type MatchingPreferences{
 `;
 
 export const typeDef = matchingDemographicsType + matchingPreferencesType;
+
+const demographicSchemaObject = (multipleResponse, responseEnum) => ({
+  response: {
+    type: multipleResponse ? [String] : String,
+    enum: responseEnum,
+    index: true,
+    required: true,
+    default: multipleResponse ? [] : 'PREFER_NOT_TO_SAY',
+  },
+  visible: { type: Boolean, required: true, default: false },
+  userHasResponded: { type: Boolean, required: true, default: false },
+});
+
+const ethnicityEnum = [
+  'AMERICAN_INDIAN',
+  'BLACK_AFRICAN_DESCENT',
+  'EAST_ASIAN',
+  'HISPANIC_LATINO',
+  'MIDDLE_EASTERN',
+  'PACIFIC_ISLANDER',
+  'SOUTH_ASIAN',
+  'WHITE_CAUCASIAN',
+  'OTHER',
+  'PREFER_NOT_TO_SAY',
+];
+const educationLevelEnum = [
+  'HIGH_SCHOOL',
+  'UNDERGRAD',
+  'POSTGRAD',
+  'PREFER_NOT_TO_SAY',
+];
+const religionEnum = [
+  'BUDDHIST',
+  'CATHOLIC',
+  'CHRISTIAN',
+  'HINDU',
+  'JEWISH',
+  'MUSLIM',
+  'SPIRITUAL',
+  'AGNOSTIC',
+  'ATHEIST',
+  'OTHER',
+  'PREFER_NOT_TO_SAY',
+];
+const politicalViewEnum = [
+  'LIBERAL',
+  'MODERATE',
+  'CONSERVATIVE',
+  'OTHER',
+  'PREFER_NOT_TO_SAY',
+];
+const habitsEnum = [
+  'YES',
+  'SOMETIMES',
+  'NO',
+  'PREFER_NOT_TO_SAY',
+];
+
+const EthnicityInfoSchema = new Schema(demographicSchemaObject(true, ethnicityEnum));
+const EducationLevelSchema = new Schema(demographicSchemaObject(false, educationLevelEnum));
+const ReligionInfoSchema = new Schema(demographicSchemaObject(true, religionEnum));
+const PoliticsInfoSchema = new Schema(demographicSchemaObject(false, politicalViewEnum));
+const DrinkingInfoSchema = new Schema(demographicSchemaObject(false, habitsEnum));
+const SmokingInfoSchema = new Schema(demographicSchemaObject(false, habitsEnum));
+const CannabisInfoSchema = new Schema(demographicSchemaObject(false, habitsEnum));
+const DrugsInfoSchema = new Schema(demographicSchemaObject(false, habitsEnum));
 
 export const MatchingDemographicsSchema = new Schema({
   gender: {
@@ -40,6 +211,46 @@ export const MatchingDemographicsSchema = new Schema({
   location: {
     type: LocationSchema,
     required: false,
+  },
+  ethnicity: {
+    type: EthnicityInfoSchema,
+    required: true,
+    default: EthnicityInfoSchema,
+  },
+  educationLevel: {
+    type: EducationLevelSchema,
+    required: true,
+    default: EducationLevelSchema,
+  },
+  religion: {
+    type: ReligionInfoSchema,
+    required: true,
+    default: ReligionInfoSchema,
+  },
+  politicalView: {
+    type: PoliticsInfoSchema,
+    required: true,
+    default: PoliticsInfoSchema,
+  },
+  drinking: {
+    type: DrinkingInfoSchema,
+    required: true,
+    default: DrinkingInfoSchema,
+  },
+  smoking: {
+    type: SmokingInfoSchema,
+    required: true,
+    default: SmokingInfoSchema,
+  },
+  cannabis: {
+    type: CannabisInfoSchema,
+    required: true,
+    default: CannabisInfoSchema,
+  },
+  drugs: {
+    type: DrugsInfoSchema,
+    required: true,
+    default: DrugsInfoSchema,
   },
 }, { timestamps: true });
 
