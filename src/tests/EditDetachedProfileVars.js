41e6df43e29e113bfe3dddabffff75c023ddba2b
briangu33 <briangu33@gmail.com>
2019-05-25 20:49:14 -0400

    editing endorsements backend

--- src/tests/EditDetachedProfileVars.js
+++ src/tests/EditDetachedProfileVars.js
@@ -1,115 +1,28 @@
 import {
-  BRIAN_ID,
-  BRIAN_PROFILE_SAMMI_D_ID, JOSH_ID, SAMMI_BOAST1_ID,
-  SAMMI_ID, SAMMI_PROFILE_JOSH_D_ID,
-  SOPHIA_PROFILE_UMA_D_ID,
-  UMA_ID,
-  UMA_PROFILE_BRIAN_D_ID,
+  JOSH_ID, QUESTION1_ID,
+  SAMMI_PROFILE_JOSH_D_ID, SAMMI_QR1_ID,
 } from './TestsContants';
-import { FAKE_IMAGE_1, FAKE_IMAGE_2 } from './FakeImages';
-
-// changes to almost all fields, as well as additions
-export const EDIT_BRIAN1_DETACHED_PROFILE_VARIABLES = {
-  editDetachedProfileInput: {
-    _id: BRIAN_PROFILE_SAMMI_D_ID,
-    lastName: 'Gu',
-    creatorUser_id: SAMMI_ID,
-    bio: {
-      author_id: SAMMI_ID,
-      authorFirstName: 'Sammi',
-      content: 'A very strange mans',
-    },
-    boasts: [
-      {
-        author_id: SAMMI_ID,
-        authorFirstName: 'Sammi',
-        content: 'he turned MIT into a meme',
-      },
-    ],
-    roasts: [
-      {
-        author_id: SAMMI_ID,
-        authorFirstName: 'Sammi',
-        content: 'The kind of guy who would make a dating app to get a date',
-      },
-    ],
-    images: [FAKE_IMAGE_1, FAKE_IMAGE_2],
-  },
-};
-
-// minor changes: delete a boast
-export const EDIT_UMA1_DETACHED_PROFILE_VARIABLES = {
-  editDetachedProfileInput: {
-    _id: UMA_PROFILE_BRIAN_D_ID,
-    creatorUser_id: BRIAN_ID,
-    boasts: [
-      {
-        author_id: BRIAN_ID,
-        authorFirstName: 'Uma',
-        content: 'Gyms religiously',
-      },
-    ],
-    roasts: [],
-  },
-};
 
 export const EDIT_SAMMI1_DETACHED_PROFILE_VARIABLES = {
   editDetachedProfileInput: {
     _id: SAMMI_PROFILE_JOSH_D_ID,
     creatorUser_id: JOSH_ID,
-    boasts: [
+    questionResponses: [
       {
+        _id: SAMMI_QR1_ID,
         author_id: JOSH_ID,
         authorFirstName: 'Josh',
-        content: 'Sammi boast 0',
-      },
-      {
-        _id: SAMMI_BOAST1_ID,
-        author_id: JOSH_ID,
-        authorFirstName: 'Josh',
-        content: 'Sammi boast 1',
-      },
-    ],
-  },
-};
-
-// no changes
-export const EDIT_SOPHIA2_DETACHED_PROFILE_VARIABLES = {
-  editDetachedProfileInput: {
-    _id: SOPHIA_PROFILE_UMA_D_ID,
-    creatorUser_id: UMA_ID,
-    boasts: [
-      {
-        author_id: UMA_ID,
-        authorFirstName: 'Uma',
-        content: 'This girl can juggle!!',
+        question_id: QUESTION1_ID,
+        question: {
+          _id: QUESTION1_ID,
+          questionText: 'How did they end up on the cover of Forbes?',
+          questionTextWithName: 'How did {{name}} end up on the cover of Forbes?',
+          questionType: 'freeResponse',
+          suggestedResponses: [],
+          tags: [],
+        },
+        responseBody: 'sammi response 1-1 edit 1',
       },
     ],
-    roasts: [
-      {
-        author_id: UMA_ID,
-        authorFirstName: 'Uma',
-        content: 'Likes sleep more than she\'ll like you',
-      },
-    ],
-    questionResponses: [],
-    vibes: [
-      {
-        author_id: UMA_ID,
-        authorFirstName: 'Uma',
-        content: 'Zesty',
-      },
-      {
-        author_id: UMA_ID,
-        authorFirstName: 'Uma',
-        content: 'BANANAS',
-      },
-      {
-        author_id: UMA_ID,
-        authorFirstName: 'Uma',
-        content: 'Spicy',
-      },
-    ],
-    images: [],
   },
 };
