c3ee03469d207c8ea748cc7c21a790ca670b0785
briangu33 <briangu33@gmail.com>
2019-05-30 03:11:21 -0400

    update your own prompts, and limit 5 visible prompts

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -111,6 +111,8 @@ input UpdateUserInput {
   age: Int
   birthdate: String
   
+  questionResponses: [QuestionUserResponseInput!]
+  
   ethnicity: [EthnicityEnum]
   ethnicityVisible: Boolean
   educationLevel: EducationLevelEnum
