c4c3d0fab3fc1ea8ea840b4a5f702080ef8ab803
Avery Lamp <averylamp@gmail.com>
2019-06-18 23:55:15 +0000

    New start scripts

--- pm2-admin-panel.sh
+++ pm2-admin-panel.sh
@@ -2,4 +2,4 @@
 sh admin-panel/start-admin.sh
 cd $(dirname $0)
 pm2 reload pm2.config.adminpanel.json 
-
+ADMIN_SERVER_URL=http://sloths.mit.edu:1235/graphql pm2 serve --name  Admin-Panel admin-panel/build 9091 --spa
