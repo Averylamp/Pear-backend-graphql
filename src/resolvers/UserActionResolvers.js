82ff8dec313674ab191274d8aab9e2a5fdb1fe34
briangu33 <briangu33@gmail.com>
2019-06-19 13:16:39 -0400

    add remaining actions

---
+++ src/resolvers/UserActionResolvers.js
@@ -0,0 +1,23 @@
+import { EventModel } from '../models/EventModel';
+import { User } from '../models/UserModel';
+
+const errorLog = require('debug')('error:UserActionResolvers');
+
+export const resolvers = {
+  Query: {
+    getEventByCode: async (_source, { code }) => EventModel.findOne({ code }).exec(),
+    getEventById: async (_source, { event_id }) => EventModel.findById(event_id).exec(),
+  },
+  UserActionSummary: {
+    user: async ({ user_id }) => User.findById(user_id),
+  },
+  UserAction: {
+    actor: async ({ actor_id }) => User.findById(actor_id),
+    users: async ({ user_ids }) => (
+      Promise.all(user_ids.map(user_id => User.findById(user_id))).catch((err) => {
+        errorLog(`error recording user action ${err}`);
+        return [];
+      })
+    ),
+  },
+};
