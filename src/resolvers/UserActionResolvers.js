e388c7f31ee386ff54ef641f6b38c9b065ea72d5
briangu33 <briangu33@gmail.com>
2019-06-20 15:50:09 -0400

    custom actions

--- src/resolvers/UserActionResolvers.js
+++ src/resolvers/UserActionResolvers.js
@@ -1,5 +1,8 @@
 import { EventModel } from '../models/EventModel';
 import { User } from '../models/UserModel';
+import { recordAction } from '../models/UserActionModel';
+import { COULDNT_RECORD_ACTION } from './ResolverErrorStrings';
+import { generateSentryErrorForResolver } from '../SentryHelper';
 
 const errorLog = require('debug')('error:UserActionResolvers');
 
@@ -7,6 +10,53 @@ export const resolvers = {
   Query: {
     getEventByCode: async (_source, { code }) => EventModel.findOne({ code }).exec(),
     getEventById: async (_source, { event_id }) => EventModel.findById(event_id).exec(),
+  },
+  Mutation: {
+    recordCustomAction: async (_source, { userActionInput }) => {
+      try {
+        const {
+          user_id,
+          actor_id,
+          user_ids,
+          description,
+          actionType,
+        } = userActionInput;
+        const action = {
+          actor_id,
+          user_ids,
+          description,
+          actionType,
+          timestamp: new Date(),
+        };
+        const actionRes = await recordAction({
+          user_id,
+          action,
+        });
+        if (!actionRes) {
+          return {
+            success: true, // don't alert error on client
+            message: COULDNT_RECORD_ACTION,
+          };
+        }
+        return {
+          success: true,
+          action: actionRes,
+        };
+      } catch (e) {
+        errorLog(`error occurred while creating user: ${e}`);
+        generateSentryErrorForResolver({
+          resolverType: 'mutation',
+          routeName: 'recordCustomAction',
+          args: { userActionInput },
+          errorMsg: e,
+          errorName: COULDNT_RECORD_ACTION,
+        });
+        return {
+          success: true, // don't alert error on client
+          message: COULDNT_RECORD_ACTION,
+        };
+      }
+    },
   },
   UserActionSummary: {
     user: async ({ user_id }) => User.findById(user_id),
