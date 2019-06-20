5fa740c4f8ca5e3163e2c81ff94656af8be0d226
briangu33 <briangu33@gmail.com>
2019-06-20 18:32:36 -0400

    add notifications enabled field

--- src/constants.js
+++ src/constants.js
@@ -61,4 +61,6 @@ export const performingMigration1 = process.env.TASK === 'migration1';
 
 export const performingMigration2 = process.env.TASK === 'migration2';
 
+export const performingMigration062019 = process.env.TASK === 'migration062019';
+
 export const verbose = process.env.VERBOSE ? process.env.VERBOSE : false;
